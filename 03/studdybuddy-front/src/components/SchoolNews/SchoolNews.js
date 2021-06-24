1cc3e8a13f3f0fd650a0c9349e3a11d5796a4888
Olga Sowinska <olga.sowinska00@gmail.com>
2021-06-24 11:17:24 +0200

    dziwne rzeczy sie tu dziejo

--- 03/studdybuddy-front/src/components/SchoolNews/SchoolNews.js
+++ 03/studdybuddy-front/src/components/SchoolNews/SchoolNews.js
@@ -1,45 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Article, ArticleImage, Articles, Title, Wrapper } from 'components/SchoolNews/SchoolNews.styles';
 
+const query = `{
+  allArticles {
+    title
+    content
+    image {
+      alt
+      url
+    }
+  }
+}`;
 
-const SchoolNews = () => (
-  <Wrapper>
-    <Title>Gazetka szkolna</Title>
-    <Articles>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-        <h3>Wymiana komputerów</h3>
-        <p>Już niebawem w sali informatycznej 118
-          pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-          <h3>Wymiana komputerów</h3>
-          <p>Już niebawem w sali informatycznej 118
-            pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-      <Article>
-        <ArticleImage>
-          <img src="https://picsum.photos/250/250" />
-        </ArticleImage>
-        <div>
-          <h3>Wymiana komputerów</h3>
-          <p>Już niebawem w sali informatycznej 118
-            pojawi się kilka nowych urządzeń!</p>
-        </div>
-      </Article>
-    </Articles>
-  </Wrapper>
-);
+const DATO_TOKEN = `637c0f02361ac0d8fc533a1bacb2eb`;
+
+const SchoolNews = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://graphql.datocms.com/`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${DATO_TOKEN}`,
+      },
+      body: JSON.stringify({
+        query
+      })
+    }).then(response => response.json())
+      .then(({ data }) => setArticles(data.allArticles))
+      .catch(error => console.log(error))
+  }, []);
+
+  //Dodać przycisk "czytaj dalej" w poszczególnych artykułach żeby tyle tekstu się nie wyświetlało od razu ale żeby można było go rozwinąć
+  return (
+    <Wrapper>
+      <Title>Gazetka szkolna</Title>
+      <Articles>
+        {articles ? articles.map(article => (
+          <Article key={article.title}>
+            <ArticleImage>
+              <img src={article.image.url} alt={article.image.alt} />
+            </ArticleImage>
+            <div>
+              <h3>{article.title}</h3>
+              <p>{article.content}</p>
+            </div>
+          </Article>
+        )) : <h3>Brak artykułów</h3>}
+      </Articles>
+    </Wrapper>
+  );
+};
 
 SchoolNews.propTypes = {};
 
